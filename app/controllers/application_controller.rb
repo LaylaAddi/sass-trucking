c525ca61a16a1280608a799c1acf44df6026ac69
steve chesnowitz <steve@chesnowitz.com>
2016-11-11 18:47:04 +0000

    merger clean up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :driver_list 
-  around_filter :user_time_zone, if: :current_user
+  around_filter :user_time_zone, if: :user
   
   
   def driver_list 
     @driver_list = DriverUser.where(["employment_status = ?", "active"])  
   end
   
-  devise_group :currentuser, contains: [:hrc_user, :shipper_user, :driver_user] 
+  devise_group :user, contains: [:hrc_user, :shipper_user, :driver_user] 
   devise_group :hrc_user, contains: [:hrc_user] 
   before_action :authenticate_user!
 
