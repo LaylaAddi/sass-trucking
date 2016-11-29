1b6622a937401da2dfc41b6ce54f7524a2063825
steve chesnowitz <steve@chesnowitz.com>
2016-11-29 01:37:39 +0000

    oerder drivers first name main nav application controller edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   
   
   def driver_list 
-    @driver_list = DriverUser.where(["employment_status = ?", "active"]).order(:first_name)  
+    @driver_list = DriverUser.where(["employment_status = ?", "active"]).order('first_name ASC')   
   end
   
   devise_group :user, contains: [:hrc_user, :shipper_user, :driver_user] 
--- gp
+++ gp
@@ -1,5 +1,5 @@
 git add -A
-git commit -m "2waytext"
+git commit -m "oerder drivers first name main nav application controller edit"
 git push -u origin master 
 git push dokku master
 
