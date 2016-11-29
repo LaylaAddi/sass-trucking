3fd34b8f9e901bfd34050dd8af05956eb1c9324b
steve chesnowitz <steve@chesnowitz.com>
2016-11-29 00:00:24 +0000

    2waytext

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :driver_list 
   around_filter :user_time_zone, if: :current_user
   
