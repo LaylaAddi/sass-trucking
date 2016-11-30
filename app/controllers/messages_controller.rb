cf1f981313bf40e815d6b8feedfb17a4068b7446
steve chesnowitz <steve@chesnowitz.com>
2016-11-30 11:56:47 +0000

    skip_before_action :authenticate_user!

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,7 @@
 class MessagesController < ApplicationController
   include ApplicationHelper
  skip_before_filter :verify_authenticity_token
+ skip_before_action :authenticate_user! 
  
   def index
     @messages = Message.all
